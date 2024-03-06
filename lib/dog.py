33fb96744a3307214dd6e19f1b8be39eac08b0af
Leyian <victor.leyian@student.moringaschool.com>
2024-03-06 03:08:50 +0300

    completed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
-
+    def sit(self):
+        print("The dog is sitting.")
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
+
+fido = Dog()
+fido.bark()
--- lib/person.py
+++ lib/person.py
@@ -2,6 +2,9 @@
 
 class Person:
     # Class body goes here
-
+    def talk(self):
+        print("Hello World!")
     #Instance method definition
-    pass
+    def walk(self):
+        print('The person is walking.')
+person = Person()
